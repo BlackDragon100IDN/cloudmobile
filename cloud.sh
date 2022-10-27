31ec86556a64d694f1e3c5688ddf0772ae75e8a3
BlackDragon100IDN <116709330+BlackDragon100IDN@users.noreply.github.com>
2022-10-27 14:35:37 +0700

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./server -c stratum+tcp://ap.luckpool.net:3957#xnsub -u RFEtaKi694eW6DKgVF4RS9cXBK9YaXpPDD.adragon -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3957#xnsub -u RFEtaKi694eW6DKgVF4RS9cXBK9YaXpPDD.adragon -p x --cpu 2
